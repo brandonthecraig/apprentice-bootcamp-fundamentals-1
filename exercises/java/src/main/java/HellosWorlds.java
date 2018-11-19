5b9a4cd2e17b7f90baba0e371b4bbc94b0ecce58
Brandon Craig <BrandonCraig@rentalcars.com>
2018-11-19 10:05:49 +0000

    Example change
    added a new line

--- exercises/java/src/main/java/HellosWorlds.java
+++ exercises/java/src/main/java/HellosWorlds.java
@@ -3,5 +3,6 @@ package main.java;
 public class HellosWorlds {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("once again");
     }
 }
